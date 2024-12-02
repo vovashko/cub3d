2e704fca26ebeda057658b9a555dd0604b5aac4d
vovashko <vshkonda@f1r1s19.codam.nl>
2024-12-02 16:52:43 +0100

    found a way to check the wall enclosure without introduction of the flood_fill or contour matching. in order to handle the edge case when the player starting position is in the wall without any leading spaces around, we can add the space at the beginning as the first character of every row and also as the endig character until all rows are same width. probably not super efficient, but we will see later

--- map_validation.c
+++ map_validation.c
@@ -6,7 +6,7 @@
 /*   By: vshkonda <vshkonda@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/02 12:36:50 by vshkonda      #+#    #+#                 */
-/*   Updated: 2024/12/02 16:21:47 by vshkonda      ########   odam.nl         */
+/*   Updated: 2024/12/02 16:50:34 by vshkonda      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,16 +77,51 @@ bool check_starting_pos(char **map, int height, t_player *player)
 	return (true);
 }
 
+bool is_surrounded_by_walls_or_spaces(char **map, size_t x, int y, int rows, size_t cols)
+{
+    // Check the left, right, top, and bottom neighbors
+    if (x > 0 && map[y][x - 1] != '1' && map[y][x - 1] != ' ') // Left
+        return false;
+    if (x < cols - 1 && map[y][x + 1] != '1' && map[y][x + 1] != ' ') // Right
+        return false;
+    if (y > 0 && map[y - 1][x] != '1' && map[y - 1][x] != ' ') // Top
+        return false;
+    if (y < rows - 1 && map[y + 1][x] != '1' && map[y + 1][x] != ' ') // Bottom
+        return false;
+
+    return true;
+}
+
+// Function to check if the map is properly enclosed
+bool check_enclosure(char **map, int rows, size_t cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            if (map[i][j] == ' ')
+            {
+                // Check if the space is surrounded by walls or other spaces
+                if (!is_surrounded_by_walls_or_spaces(map, j, i, rows, cols))
+                {
+                    printf("Invalid map: space at (%d, %zu) is not surrounded by walls or other spaces\n", i, j);
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
 
 
 // Main for testing
 int main()
 {
     char *map[] = {
-        "    111111       ",
-        "11110000001N01111",
-        "10000000101      ",
-        "1111111111     00"
+        "     111111111    ",
+        " 11110000001111111",
+        " 1S00000101       ",
+        " 11111111111      "
     };
 
     t_player *player = malloc(sizeof(t_player));
@@ -107,7 +142,10 @@ int main()
     size_t cols = get_map_width(map, rows);
     printf("Map width: %zu\n", cols);
 
-
+	if (check_enclosure(map, rows, cols))
+        printf("The map is valid and enclosed!\n");
+    else
+        printf("The map is invalid (not properly enclosed)!\n");
     free(player);
 
     return 0;
