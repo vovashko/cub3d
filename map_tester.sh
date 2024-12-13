05420a7077d4cd8d48d1cac9e6fda5515877fbdf
vovashko <vshkonda@f1r1s19.codam.nl>
2024-12-13 13:28:57 +0100

    added script tester for maps, commented out the ones that don't have issues

---
+++ map_tester.sh
@@ -0,0 +1,47 @@
+
+# ./cub3d maps/bad/color_invalid_rgb.cub
+# ./cub3d maps/bad/color_missing.cub
+# ./cub3d maps/bad/color_missing_ceiling_rgb.cub
+./cub3d maps/bad/color_missing_floor_rgb.cub
+# ./cub3d maps/bad/color_none.cub
+# ./cub3d maps/bad/empty.cub
+# ./cub3d maps/bad/file_letter_end.cub
+# ./cub3d maps/bad/forbidden.cub
+./cub3d maps/bad/map_first.cub # Segfault
+./cub3d maps/bad/map_middle.cub
+# ./cub3d maps/bad/map_missing.cub
+# ./cub3d maps/bad/map_only.cub
+./cub3d maps/bad/map_too_small.cub
+# ./cub3d maps/bad/player_multiple.cub
+./cub3d maps/bad/player_none.cub
+# ./cub3d maps/bad/player_on_edge.cub
+./cub3d maps/bad/textures_dir.cub
+# ./cub3d maps/bad/textures_duplicates.cub
+./cub3d maps/bad/textures_forbidden.cub
+./cub3d maps/bad/textures_invalid.cub
+./cub3d maps/bad/textures_missing.cub
+./cub3d maps/bad/textures_none.cub
+./cub3d maps/bad/textures_not_xpm.cub
+./cub3d maps/bad/wall_hole_east.cub
+./cub3d maps/bad/wall_hole_north.cub
+./cub3d maps/bad/wall_hole_south.cub
+./cub3d maps/bad/wall_hole_west.cub
+./cub3d maps/bad/wall_none.cub
+
+./cub3d maps/good/cheese_maze.cub
+./cub3d maps/good/creepy.cub
+./cub3d maps/good/dungeon.cub
+./cub3d maps/good/library.cub
+./cub3d maps/good/matrix.cub
+./cub3d maps/good/sad_face.cub
+./cub3d maps/good/square_map.cub
+./cub3d maps/good/subject_map.cub
+./cub3d maps/good/test_map.cub
+./cub3d maps/good/test_map_hole.cub
+./cub3d maps/good/test_pos_bottom.cub
+./cub3d maps/good/test_pos_left.cub
+./cub3d maps/good/test_pos_right.cub
+./cub3d maps/good/test_pos_top.cub
+./cub3d maps/good/test_textures.cub
+./cub3d maps/good/test_whitespace.cub
+./cub3d maps/good/works.cub
