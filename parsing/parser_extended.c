6deaa0ca09574e4ed5dbb0440be94298839a6ee9
Paulius Minialga <pminialg@f1r1s17.codam.nl>
2025-01-09 14:45:24 +0100

    Fixed get_file_data according to the norm

---
+++ parsing/parser_extended.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parser_extended.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pminialg <pminialg@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/09 14:26:33 by pminialg      #+#    #+#                 */
+/*   Updated: 2025/01/09 14:43:31 by pminialg      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+bool	process_line(t_map_file_data *mfd, int *map_started, int fd, char *line)
+{
+	char	*trimmed_line;
+
+	trimmed_line = skip_spaces(line);
+	printf("trimmed line - %s", trimmed_line);
+	if (process_empty_line(*map_started, fd, line))
+		return (true);
+	if (is_config_line(trimmed_line) && !(*map_started))
+		parse_config_line(mfd, trimmed_line);
+	else if (ft_strchr("01NSEW", *trimmed_line))
+	{
+		if (!(*map_started))
+			*map_started = 1;
+		grow_map(mfd, trimmed_line);
+	}
+	else
+	{
+		free(line);
+		return (handle_error("Invalid line encountered"), false);
+	}
+	free(line);
+	return (true);
+}
